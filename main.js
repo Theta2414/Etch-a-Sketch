f3043314ab9b2af4d3186fa3c81268b4fdd127b4
Theta <169332632+Theta2414@users.noreply.github.com>
2025-08-03 13:26:38 +0700

    Completed version 1.0

--- main.js
+++ main.js
@@ -3,26 +3,25 @@ const display = document.querySelector(".display");
 const reset = document.querySelector(".reset");
 
 //false means draw on hover
-let drawOnHoverAndPress = false;
+let drawOnPressAndHold = false;
 const drawMode1 = document.querySelector("input[value='-1']");
 const drawMode2 = document.querySelector("input[value='1']");
 
-let colorful = false;
+let color = "#000000ff";
+let isColorful = false;
 const colorMode1 = document.querySelector("input[value='-2']");
 const colorMode2 = document.querySelector("input[value='2']");
+const customColor = document.querySelector("input[type='color']");
 
 const erase = document.querySelector("#erase");
 const eraseBtn = document.querySelector(".erase-label");
 const eraseAllBtn = document.querySelector(".erase-all-btn");
-let eraseOnHoverAndPress = false;
+let eraseOnPressAndHold = false;
 let isErasing = false;
 const eraseMode1 = document.querySelector("input[value='-3']");
 const eraseMode2 = document.querySelector("input[value='3']");
 
 let isMouseDown = false;
-let mouseHover = true;
-let colorGray = true;
-let mouseHoverErase = true;
 
 let size = null;
 
@@ -36,8 +35,8 @@ function startNew() {
     size = null;
     display.textContent = "";
     while (!(1 <= size && size <= 100)) {
-        size = prompt("What is the size you want? Enter a number from 1 to 100. Enter stop to break");
-        if (size == "stop") break;
+        size = prompt("What is the size you want? Enter a number from 1 to 100. Enter stop to break. 10-40 to get best experience");
+        if (size === "stop") break;
     }
     if (1 <= size && size <= 100) {
         size = +size;
@@ -64,16 +63,20 @@ function clear() {
 
 startNewGame.addEventListener("click", startNew);
 
-drawMode1.addEventListener("click", () => drawOnHoverAndPress = false);
-drawMode2.addEventListener("click", () => drawOnHoverAndPress = true);
+drawMode1.addEventListener("click", () => drawOnPressAndHold = false);
+drawMode2.addEventListener("click", () => drawOnPressAndHold = true);
 
-eraseMode1.addEventListener("click", () => eraseOnHoverAndPress = false);
-eraseMode2.addEventListener("click", () => eraseOnHoverAndPress = true);
+customColor.addEventListener("change", () => color = customColor.value);
+colorMode1.addEventListener("click", () => isColorful = false);
+colorMode2.addEventListener("click", () => isColorful = true);
 
-erase.addEventListener("change", checkEraseMode)
+eraseMode1.addEventListener("click", () => eraseOnPressAndHold = false);
+eraseMode2.addEventListener("click", () => eraseOnPressAndHold = true);
+
+erase.addEventListener("change", checkEraseMode);
 
 eraseAllBtn.addEventListener("click", () => {
-    display.childNodes.forEach(item => item.style.backgroundColor = "#fbf2c4");
+    display.childNodes.forEach(item => item.style.backgroundColor = "#f0eddeff");
 });
 
 reset.addEventListener("click", clear);
@@ -82,10 +85,14 @@ display.addEventListener("mousedown", (e) => {
     if (!(isMouseDown) && e.target.classList.contains("pixel")) {
         isMouseDown = true;
         e.preventDefault();
-        if (!isErasing) {
-            e.target.style.backgroundColor = "#c7522a";
-        } else {
-            e.target.style.backgroundColor = "#fbf2c4";
+        if (!isErasing && drawOnPressAndHold) {
+            if (!isColorful) {
+                e.target.style.backgroundColor = color;
+            } else {
+                e.target.style.backgroundColor = `rgb(${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)})`;
+            }
+        } else if (isErasing && eraseOnPressAndHold) {
+            e.target.style.backgroundColor = "#f0eddeff";
         }
     }
 });
@@ -97,21 +104,29 @@ document.addEventListener("mouseup", (e) => {
 });
 
 display.addEventListener("mouseover", (e) => {
-    if (!drawOnHoverAndPress && !isErasing) {
+    if (!drawOnPressAndHold && !isErasing) {
         if (e.target.classList.contains("pixel")) {
-            e.target.style.backgroundColor = "#c7522a";
+            if (!isColorful) {
+                e.target.style.backgroundColor = color;
+            } else {
+                e.target.style.backgroundColor = `rgb(${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)})`;
+            }
         }
-    } else if (drawOnHoverAndPress && !isErasing && isMouseDown) {
+    } else if (drawOnPressAndHold && !isErasing && isMouseDown) {
         if (e.target.classList.contains("pixel")) {
-            e.target.style.backgroundColor = "#c7522a";
+            if (!isColorful) {
+                e.target.style.backgroundColor = color;
+            } else {
+                e.target.style.backgroundColor = `rgb(${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)})`;
+            }
         }
-    } else if (isErasing && !eraseOnHoverAndPress) {
+    } else if (isErasing && !eraseOnPressAndHold) {
         if (e.target.classList.contains("pixel")) {
-            e.target.style.backgroundColor = "#fbf2c4";
+            e.target.style.backgroundColor = "#f0eddeff";
         }
-    } else if (isErasing && eraseOnHoverAndPress && isMouseDown) {
+    } else if (isErasing && eraseOnPressAndHold && isMouseDown) {
         if (e.target.classList.contains("pixel")) {
-            e.target.style.backgroundColor = "#fbf2c4";
+            e.target.style.backgroundColor = "#f0eddeff";
         }
     }
 });
